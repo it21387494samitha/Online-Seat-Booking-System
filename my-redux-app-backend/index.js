6cbe6b430cf8294f311ff9d2540edc6ff6681fae
SamithaDhananjaya <it21387494@my.sliit.lk>
2024-09-06 09:45:04 +0530

    added backend env

--- my-redux-app-backend/index.js
+++ my-redux-app-backend/index.js
@@ -1,19 +1,29 @@
-// index.js
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB', err));
+
+// Import Routes
+
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// Sample Route
-app.get('/', (req, res) => {
-  res.send('Hello from Node.js Backend!');
-});
+// Use Routes
+
 
 // Start the Server
 app.listen(PORT, () => {
