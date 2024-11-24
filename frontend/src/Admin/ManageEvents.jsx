57aff5f6afbf4c70947a511629d76c225660c6e2
SamithaDhananjaya <101282783+it21387494samitha@users.noreply.github.com>
2024-11-24 17:12:28 +0530

    Update ManageEvents.jsx

--- frontend/src/Admin/ManageEvents.jsx
+++ frontend/src/Admin/ManageEvents.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Navigate, useNavigate } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 
 function ManageEvents() {
   const [events, setEvents] = useState([]);
