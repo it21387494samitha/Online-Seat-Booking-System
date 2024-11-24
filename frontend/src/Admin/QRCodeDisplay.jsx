97845a762ce6ce1f8be571de3bb2c3899776613e
SamithaDhananjaya <101282783+it21387494samitha@users.noreply.github.com>
2024-11-24 17:13:04 +0530

    Update QRCodeDisplay.jsx

--- frontend/src/Admin/QRCodeDisplay.jsx
+++ frontend/src/Admin/QRCodeDisplay.jsx
@@ -17,7 +17,7 @@ const QRCodeDisplay = () => {
     const fetchEvents = async () => {
       try {
         const token = localStorage.getItem('token');
-        const response = await axios.get('http://localhost:5000/api/events', {
+        const response = await axios.get('http://localhost:5000/events/allad', {
           headers: {
             Authorization: `Bearer ${token}`,
           },
@@ -55,7 +55,7 @@ const QRCodeDisplay = () => {
   const handleDeleteClick = async (eventId) => {
     try {
       const token = localStorage.getItem('token');
-      await axios.delete(`http://localhost:5000/api/events/soft/${eventId}`, {
+      await axios.delete(`http://localhost:5000/events/soft/${eventId}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
