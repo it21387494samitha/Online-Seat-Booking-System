22727f46d790aad85cb84f6bdf94ca8475da0f64
SamithaDhananjaya <dhananjayasamitha68@gmail.com>
2024-11-25 12:46:02 +0530

    changes done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React  from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google'; // Import the provider
 import Register from '../src/Pages/Login/RegisterPage';
@@ -16,19 +16,20 @@ import Sidebar from './Component/SideBar';
 import Profile from './User/UserProfile';
 import ProfileImageUpload from './User/ProfileImageUpload';
 import BookingHistory from './Pages/History/BookingHistory';
-import Settings from './Pages/Setting/Setting';
+import UserSettings from './Pages/Setting/Setting';
 import AdminSeatBooking from './Admin/AdminSeatBooking';
 import ManageUsers from './Admin/ManageUsers';
 import QR from './Admin/QRCodeDisplay';
 import Service from './Pages/Service/Service';
+import GoogleSuccess from './Pages/Login/GoogleSuccess';
+import EditEvent from './Admin/EventEdit';
+import Contact from './Pages/Contact/Contact';
+import AboutUs from './Pages/AboutUs/AboutUs';
 
 function App() {
-  // Manage sidebar open/collapse state
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
+  
+
 
   
 
@@ -48,14 +49,19 @@ function App() {
             <Route path="/allseat" element={<AdminSeatBooking />} />
             <Route path="/manage-users" element={<ManageUsers />} />
             <Route path="/QR" element={<QR />} />
+            <Route path='/admin/edit-event/:eventId' element={<EditEvent/>} />
 
             {/* Customer Routes */}
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/auth/google/success" element={<GoogleSuccess/>} />
             <Route path="/" element={<CustomerDash />} />
             <Route path="/mevent" element={<ManageEvents />} />
             <Route path="/list" element={<EventList />} />
             <Route path='/services' element={<Service />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/about' element={<AboutUs/>}/>
+            <Route path='/settings' element={<UserSettings/>} />
 
             {/* Booking Route */}
             <Route path="/booking/:eventId" element={<SeatBooking />} />
@@ -63,8 +69,8 @@ function App() {
             <Route path='/p' element={<ProfileImageUpload />} />
             <Route path='/history' element={<BookingHistory />} />
             
-            {/* Setting Route */}
-            <Route path='/settings' element={<Settings />} />
+        
+           
           </Routes>
         </div>
       </Router>
--- frontend/src/Pages/Login/Login.js
+++ frontend/src/Pages/Login/Login.js
@@ -1,114 +1,43 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import { auth,provider} from '../../firebase';
-// import { signInWithPopup } from 'firebase/auth';
-import { Link, useNavigate } from 'react-router-dom';
-import { FcGoogle } from 'react-icons/fc'; 
-import { FaEye, FaEyeSlash } from 'react-icons/fa'; 
+
+import { Link} from 'react-router-dom';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
 import './Login.css';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-const adminEmails = ['dhananjayasamitha68@gmail.com'];
 
-function Login() {
+function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [passwordVisible, setPasswordVisible] = useState(false); 
-  const navigate = useNavigate();
+  const [loading] = useState(false);
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
 
-  const handleGoogleLogin = async () => {
-    const auth = getAuth();
-    const provider = new GoogleAuthProvider();
-    
-    try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-  
-      // Capture user details
-      const userData = {
-        email: user.email,
-        displayName: user.displayName,
-        photoURL: user.photoURL,
-        uid: user.uid
-      };
-  
-      // Send user data to backend
-      await axios.post('http://localhost:5000/users/google-login', userData);
-  
-      // Handle further login logic, like storing token and navigating
-      
-      const token = await user.getIdToken();
-      const isAdmin = adminEmails.includes(user.email);
-      const userRole = isAdmin ? 'admin' : 'user';
-  
-      localStorage.setItem('token', token);
-      localStorage.setItem('userRole', userRole);
-  
-      console.log("Token after Google login:", token);
-      if (isAdmin) {
-        navigate('/admin');
-      } else {
-        navigate('/');
-      }
-  
-      console.log('User signed in and data sent to backend: ', user);
-  
-    } catch (error) {
-      console.error('Error during Google login:', error);
-    }
+
+  const handleGoogleLogin = () => {
+    window.open('http://localhost:5000/auth/google', '_self');
+
   };
-  
 
+  const handleEmailLogin = async () => {
 
-  
-  
-
-  const handleEmailLogin = () => {
-    if (!email || !password) {
-      alert('Please enter both email and password.');
-      return;
-    }
-
-    setLoading(true);
-    axios.post('http://localhost:5000/users/login', { email, password })
-      .then((response) => {
-       
-
-        const { token } = response.data;
-        const isAdmin = adminEmails.includes(email);
-        const userRole = isAdmin ? 'admin' : 'user';
-
-        alert('Login successful!');
-        localStorage.setItem('token', token);
-        localStorage.setItem('userRole', userRole);
-
-        if (isAdmin) {
-          navigate('/admin');
-        } else {
-          navigate('/');
-        }
-      })
-      .catch((error) => {
-        console.error('Error during email login:', error);
-        alert('Login failed. Incorrect email or password.');
-      })
-      .finally(() => setLoading(false));
   };
 
   return (
     <div className="relative flex items-center justify-center min-h-screen w-full bg-cover bg-center" style={{ backgroundImage: `url('https://images.pexels.com/photos/28291092/pexels-photo-28291092/free-photo-of-a-squirrel-climbing-up-a-tree-in-the-woods.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')` }}>
-      {/* Overlay */}
       <div className="absolute inset-0 bg-black opacity-40"></div>
 
       <div className="relative z-10 p-10 bg-white bg-opacity-25 rounded-2xl shadow-2xl backdrop-blur-md max-w-md w-full mx-4">
         <h2 className="text-3xl font-semibold text-white mb-4 text-center">Welcome Back!</h2>
         <p className="text-sm text-gray-200 mb-6 text-center">Sign in to continue</p>
 
-        <button onClick={handleGoogleLogin} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-  Sign in with Google
-</button>
+        <button
+          onClick={handleGoogleLogin}
+          className="flex items-center justify-center w-full py-3 text-blue-600 font-bold  bg-zinc-50 mb-4 rounded-lg shadow-lg transition-transform duration-200 transform hover:bg-gray-100 hover:scale-105 active:bg-gray-200 active:scale-95"
+        >
+          <FcGoogle className="mr-2 text-blue-600 text-xl" />
+          Sign in with Google
+        </button>
 
 
         <div className="space-y-4">
@@ -152,15 +81,14 @@ function Login() {
         </div>
 
         <div className="mt-4 text-center">
-          <p className="text-gray-200">Don't have an account? <Link to="/signup" className="text-pink-500 hover:underline">Sign Up</Link></p>
+          <p className="text-gray-200">Don't have an account? <Link to="/register" className="text-pink-500 hover:underline">Sign Up</Link></p>
         </div>
       </div>
 
-      {/* Decorative Circles */}
       <div className="absolute top-0 left-0 w-32 h-32 bg-purple-500 rounded-full opacity-60 blur-md"></div>
       <div className="absolute bottom-0 right-0 w-32 h-32 bg-blue-500 rounded-full opacity-60 blur-md"></div>
     </div>
   );
 }
 
-export default Login;
+export default LoginPage;
